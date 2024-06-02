ced1c0a17a1a70613b7dcd063c7e3bafb610fa96
ChidiRNweke <chidi125@gmail.com>
2024-06-02 19:58:50 +0200

    Add configuration of global logger to project root

--- dataprocessing/src/__init__.py
+++ dataprocessing/src/__init__.py
@@ -1,8 +1,16 @@
+import logging
 from dagster import Definitions, load_assets_from_modules
 from dotenv import load_dotenv
 from .orchestration import assets
 
 load_dotenv("config/.env.secret")
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 all_assets = load_assets_from_modules([assets])
