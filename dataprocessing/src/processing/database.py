c8e648d640baebc7c0ccd8096971983420082c27
ChidiRNweke <chidi125@gmail.com>
2024-06-02 17:28:50 +0200

    Make database models work properly

--- dataprocessing/src/processing/database.py
+++ dataprocessing/src/processing/database.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy import (
+    Column,
+    ForeignKeyConstraint,
+    String,
+    Integer,
+    ForeignKey,
+    Table,
+)
 
 
 class Base(DeclarativeBase):
@@ -14,11 +21,15 @@ class LanguagesModel(Base):
 
 
 languages_repository_bridge = Table(
-    "association_table",
+    "languages_repository_bridge",
     Base.metadata,
-    Column("language_name", ForeignKey("github_languages.name")),
-    Column("repository_name", ForeignKey("github_repositories.name")),
-    Column("repository_user", ForeignKey("github_repositories.user")),
+    Column("language_name", ForeignKey("github_languages.language")),
+    Column("repository_name", String(255)),
+    Column("repository_user", String(255)),
+    ForeignKeyConstraint(
+        ["repository_name", "repository_user"],
+        ["github_repositories.name", "github_repositories.user"],
+    ),
 )
 
 
@@ -27,14 +38,14 @@ class GitHubRepositoryModel(Base):
 
     name: Mapped[str] = mapped_column(String(255), primary_key=True)
     user: Mapped[str] = mapped_column(String(255), primary_key=True)
-    description: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(String(255), nullable=True)
 
     url: Mapped[str] = mapped_column(String(255))
 
     files: Mapped[list["GithubFileModel"]] = relationship(
         back_populates="repository", cascade="all, delete-orphan"
     )
-    languages: Mapped[set[LanguagesModel]] = relationship(
+    languages: Mapped[list[LanguagesModel]] = relationship(
         secondary=languages_repository_bridge
     )
 
@@ -46,7 +57,22 @@ class GithubFileModel(Base):
     name: Mapped[str] = mapped_column(String(255))
     content_url: Mapped[str] = mapped_column(String(255))
     last_modified: Mapped[datetime]
-    repository_name: Mapped[str] = mapped_column(ForeignKey("github_repositories.name"))
-    repository_user: Mapped[str] = mapped_column(ForeignKey("github_repositories.user"))
+    repository_name: Mapped[str] = mapped_column(String(255))
+    repository_user: Mapped[str] = mapped_column(String(255))
+    file_extension: Mapped[str] = mapped_column(String(255))
+    latest_version: Mapped[bool]
+    is_embedded: Mapped[bool]
 
-    repository: Mapped[GitHubRepositoryModel] = relationship(back_populates="files")
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ["repository_name", "repository_user"],
+            [
+                "github_repositories.name",
+                "github_repositories.user",
+            ],
+        ),
+    )
+
+    repository: Mapped[GitHubRepositoryModel] = relationship(
+        back_populates="files", foreign_keys=[repository_name, repository_user]
+    )
