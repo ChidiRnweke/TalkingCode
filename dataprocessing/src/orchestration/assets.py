548cd1643d8b672b9f4a440574f3b7aed1dd88e9
ChidiRNweke <chidi125@gmail.com>
2024-06-03 20:13:06 +0200

    Add datapipeline for embedding data

--- dataprocessing/src/orchestration/assets.py
+++ dataprocessing/src/orchestration/assets.py
@@ -1,13 +1,31 @@
 from dagster import asset
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from src.processing import AppConfig, save_and_persist_data
+from src.orchestration.resources import AppConfigResource
+from src.processing import save_and_persist_data, embed_and_persist_files
+from openai import AsyncOpenAI
 
 
 @asset
-def persist_data():
-    config = AppConfig.from_env()
-    client = config.get_github_client()
-    engine = create_engine(config.db_connection_string, echo=True)
+def persist_data(app_config_resource: AppConfigResource) -> None:
+    app_config = app_config_resource.get_app_config()
+    client = app_config.get_github_client()
+    engine = create_engine(app_config.db_connection_string, echo=True)
     Session = sessionmaker(engine)
     return save_and_persist_data(Session, client)
+
+
+@asset(deps=[persist_data])
+def persist_embeddings(app_config_resource: AppConfigResource):
+    app_config = app_config_resource.get_app_config()
+    engine = create_engine(app_config.db_connection_string, echo=True)
+    Session = sessionmaker(engine)
+    openai_client = AsyncOpenAI(api_key=app_config.openai_api_key)
+    github_client = app_config.get_github_client()
+    return embed_and_persist_files(
+        Session,
+        openai_client,
+        github_client,
+        app_config.model,
+        app_config.embedding_disk_path,
+    )
--- dataprocessing/src/processing/__init__.py
+++ dataprocessing/src/processing/__init__.py
@@ -2,20 +2,34 @@ import os
 from dataclasses import dataclass
 from github import Auth, Github
 from .ingestion import save_and_persist_data
+from .embedding import embed_and_persist_files
 import logging
+import json
 
-__all__ = ["AppConfig", "save_and_persist_data"]
+__all__ = [
+    "AppConfig",
+    "save_and_persist_data",
+    "embed_and_persist_files",
+    "whitelist_str_as_list",
+]
 
 
 @dataclass(frozen=True)
 class AppConfig:
+    github_token: str
     openai_api_key: str
     db_connection_string: str
+    whitelisted_extensions: list[str]
+
+    embedding_disk_path: str = "embeddings"
+    model: str = "text-embedding-3-large"
 
     @staticmethod
     def from_env() -> "AppConfig":
-        api_key = os.getenv("GITHUB_API_TOKEN")
+        github_api_key = os.getenv("GITHUB_API_TOKEN")
+        api_key = os.getenv("OPENAI_EMBEDDING_API_KEY")
         conn_string = os.getenv("DATABASE_URL")
+        whitelisted_extensions = os.getenv("WHITELISTED_EXTENSIONS")
 
         if conn_string is None:
             logging.warning("DATABASE_URL is not set. Using dev configuration.")
@@ -23,11 +37,40 @@ class AppConfig:
         else:
             conn_string = os.path.expandvars(conn_string)
 
-        if api_key is None:
-            raise ValueError("API_KEY is not set and is required to run the pipeline.")
+        if whitelisted_extensions is None:
+            raise ValueError(
+                "WHITELISTED_EXTENSIONS is not set and is required to run the pipeline."
+            )
 
-        return AppConfig(openai_api_key=api_key, db_connection_string=conn_string)
+        whitelisted_extensions = whitelist_str_as_list(whitelisted_extensions)
+
+        if github_api_key is None:
+            raise ValueError(
+                "GITHUB_API_TOKEN is not set and is required to run the pipeline."
+            )
+        if api_key is None:
+            raise ValueError(
+                "OPENAI_EMBEDDING_API_KEY is not set and is required to run the pipeline."
+            )
+
+        return AppConfig(
+            github_token=github_api_key,
+            openai_api_key=api_key,
+            db_connection_string=conn_string,
+            whitelisted_extensions=whitelisted_extensions,
+        )
 
     def get_github_client(self) -> Github:
-        auth = Auth.Token(self.openai_api_key)
+        auth = Auth.Token(self.github_token)
         return Github(auth=auth)
+
+
+def whitelist_str_as_list(whitelisted_extensions: str) -> list[str]:
+    try:
+        whitelist = json.loads(whitelisted_extensions)
+    except json.JSONDecodeError:
+        raise ValueError(
+            'WHITELISTED_EXTENSIONS must be a valid JSON array of strings. Example: \'["py", "java"]\''
+        )
+
+    return whitelist
