ea8e8954d4e0fd63b3430b58589c3f80d7ef38a3
ChidiRNweke <chidi125@gmail.com>
2024-06-04 20:37:09 +0200

    Make a reuusable setup env function

---
+++ backend/backend/__init__.py
@@ -0,0 +1,4 @@
+from shared import setup_env
+
+
+setup_env(logger_name="backend_logger")
--- shared/shared/__init__.py
+++ shared/shared/__init__.py
@@ -1,4 +1,13 @@
 from shared import database
 from shared import log
+from dotenv import load_dotenv
 
-__all__ = ["database", "log"]
+
+def setup_env(logger_name: str) -> None:
+    logger = log.setup_custom_logger(logger_name)
+    found = load_dotenv("../config/.env.secret")
+    if not found:
+        logger.warning("No .env file found")
+
+
+__all__ = ["database", "log", "setup_env"]
