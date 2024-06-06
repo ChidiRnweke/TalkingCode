92312d58ced63206489688dda6ef162367be0251
ChidiRNweke <chidi125@gmail.com>
2024-06-06 19:25:17 +0200

    chore: Update shared package version to 0.1.2

--- shared/shared/__init__.py
+++ shared/shared/__init__.py
@@ -1,13 +1,6 @@
 from shared import database
 from shared import log
-from dotenv import load_dotenv
+from shared.env import setup_env, env_var_or_default, env_var_or_throw
 
 
-def setup_env(logger_name: str) -> None:
-    logger = log.setup_custom_logger(logger_name)
-    found = load_dotenv("../config/.env.secret")
-    if not found:
-        logger.warning("No .env file found")
-
-
-__all__ = ["database", "log", "setup_env"]
+__all__ = ["database", "log", "setup_env", "env_var_or_default", "env_var_or_throw"]
---
+++ shared/shared/env.py
@@ -0,0 +1,34 @@
+import os
+from logging import Logger
+from .log import setup_custom_logger
+
+from dotenv import load_dotenv
+
+
+def setup_env(logger_name: str) -> None:
+    logger = setup_custom_logger(logger_name)
+    found = load_dotenv("../config/.env.secret")
+    if not found:
+        logger.warning("No .env file found")
+
+
+def env_var_or_default(var_name: str, default: str, log: Logger | None = None) -> str:
+    value = os.getenv(var_name)
+    if value is None:
+        if log is not None:
+            log.warning(
+                f"{var_name} environment variable is not set. Fallback to default."
+            )
+        value = default
+    return value
+
+
+def env_var_or_throw(var_name: str, log: Logger | None = None) -> str:
+    value = os.getenv(var_name)
+    if value is None:
+        if log is not None:
+            log.error(f"{var_name} environment variable is not set. Cannot start app.")
+        raise ValueError(
+            f"{var_name} environment variable is not set. Cannot start app."
+        )
+    return value
