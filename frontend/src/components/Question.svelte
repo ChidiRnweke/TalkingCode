5d6e441003ed992855a3055698849cd1e1272825
ChidiRNweke <chidi125@gmail.com>
2024-06-09 22:58:51 +0200

    chore: Update Question component layout and styling

--- frontend/src/components/Question.svelte
+++ frontend/src/components/Question.svelte
@@ -3,9 +3,11 @@
 	import Avatar from 'flowbite-svelte/Avatar.svelte';
 </script>
 
-<section class="flex flex-row place-items-start gap-x-10">
-	<P class="text-lg leading-10">
-		<slot />
-	</P>
+<div class="flex flex-row place-items-start gap-x-10">
+	<section class=" bg-primary-50 box-border p-8">
+		<P class="text-lg leading-10">
+			<slot />
+		</P>
+	</section>
 	<Avatar rounded border src="you.jpg" size="xl" class="max-w-fit p-2 text-lg font-bold"></Avatar>
-</section>
+</div>
