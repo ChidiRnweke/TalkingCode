02ca43a2675bc42f684bb193b32495917aacb692
ChidiRNweke <chidi125@gmail.com>
2024-06-10 08:03:13 +0200

    Move chat interface to landing page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
+<Navbar />
 <main class="mx-40 mt-20">
 	<slot />
 </main>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,40 @@
-<section>
-	<h1>ChatGITpt</h1>
-	<p>
-		ChatGITpt is an open-source chatbot that you can use to chat with my, Chidi Nweke's, GitHub
-		repositories.
-	</p>
-	<p>
-		The chatbot has access to all of my public repositories and can provide you with any information
-		you need about them.
-	</p>
-	<p>Try it out by asking an example question listed below:</p>
+<script lang="ts">
+	import Hr from 'flowbite-svelte/Hr.svelte';
+	import Question from '../components/Question.svelte';
+	import Answer from '../components/Answer.svelte';
+	import SendButton from '../components/SendButton.svelte';
+	import { ragClient, type PreviousContext, type RAGResponse, type inputQuery } from '$lib/client';
+	let input = '';
+	let previousContext: PreviousContext[] = [];
+	let sessionID: string | undefined = undefined;
+
+	export const getAnswer = async (question: string): Promise<RAGResponse> => {
+		const inputQuery: inputQuery = {
+			query: question,
+			session_id: sessionID,
+			previous_context: previousContext
+		};
+		const answer = await ragClient.getAnswer(inputQuery);
+		sessionID = answer.session_id;
+		previousContext = [...previousContext, { question: question, answer: answer.response }];
+		console.log(previousContext);
+		return answer;
+	};
+
+	export let sendQuestion = async () => {
+		const question = input;
+		const answer = await getAnswer(question);
+	};
+</script>
+
+<section class="mx-52 grid grid-cols-1 gap-32 pb-10">
+	{#each previousContext as ctx}
+		<div class="flex flex-col justify-between gap-y-10">
+			<Question>{ctx.question}</Question>
+			<Hr />
+			<Answer>{@html ctx.answer}</Answer>
+		</div>
+	{/each}
 </section>
+
+<SendButton bind:input bind:action={sendQuestion} />
