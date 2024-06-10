c87eed38c48d4b66d784d03519f9584d53e798c2
ChidiRNweke <chidi125@gmail.com>
2024-06-10 08:44:51 +0200

    chore: Add SuggestionCard and Suggestions components

---
+++ frontend/src/components/SuggestionCard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import P from 'flowbite-svelte/P.svelte';
+	import Button from 'flowbite-svelte/Button.svelte';
+	let action: () => void;
+</script>
+
+<Button class="p-8" on:click={action}>
+	<P class="text-sm text-primary-50"><slot /></P>
+</Button>
---
+++ frontend/src/components/Suggestions.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import SuggestionCard from './SuggestionCard.svelte';
+</script>
+
+<section class="flex md:flex-row flex-col md:gap-x-4 gap-y-4 justify-center my-44">
+	<SuggestionCard>What projects have you done in Rust?</SuggestionCard>
+	<SuggestionCard>What is your core skill set?</SuggestionCard>
+	<SuggestionCard>What is your strategy for testing your code?</SuggestionCard>
+	<SuggestionCard>Do you know anything about DevOps? How do you apply it.</SuggestionCard>
+</section>
