6c1698014becdd7d9a896502a9ad6206fc628770
ChidiRNweke <chidi125@gmail.com>
2024-06-10 07:39:40 +0200

    Make sendbutton not stick to the top of the frame

--- frontend/src/components/SendButton.svelte
+++ frontend/src/components/SendButton.svelte
@@ -32,22 +32,22 @@
 	};
 </script>
 
-<div class="sticky bottom-0 bg-white pb-8">
+<div class="sticky bottom-0 bg-white pb-4 pt-4 box-content">
 	<div class=" mx-80 flex flex-row bg-primary-300 place-items-end rounded-md">
 		<Label for="textarea-id" class="sr-only ">Your message</Label>
 		<Textarea
-			class="bg-transparent border-none placeholder:pl-4 resize-none max-h-52 focus:border-none focus:ring-0 dark:focus:ring-0 dark:focus:border-none text-lg placeholder:text-primary-900"
+			class="bg-transparent border-none placeholder:pl-4 resize-none max-h-52 py-2 focus:border-none focus:ring-0 dark:focus:ring-0 dark:focus:border-none text-lg placeholder:text-primary-900"
 			id="textarea-id"
 			placeholder="Ask me a question about my projects..."
 			name="message"
-			rows="1"
+			rows="2"
 			tabindex="0"
 			dir="auto"
 			on:input={handleInput}
 			bind:value={input}
 			on:keydown={handleKeydown}
 		/>
-		<Button class=" bg-primary-900 rounded-md mx-2 my-2" on:click={action}>
+		<Button class=" bg-primary-900 rounded-md mr-1" on:click={action}>
 			<PaperPlaneOutline class=" text-primary-100 " />
 		</Button>
 	</div>
