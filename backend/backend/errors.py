99555c9fec467e830779f1d608276c931250940b
ChidiRNweke <chidi125@gmail.com>
2024-06-11 22:35:01 +0200

    add map_errors decorator

--- backend/backend/errors.py
+++ backend/backend/errors.py
@@ -1,22 +1,63 @@
-class InfraError(Exception):
-    def __init__(self, msg: str | None = None) -> None:
-        self.msg = msg
+from typing import Callable, Generic, ParamSpec, TypeVar, Type
+from logging import getLogger
+from functools import update_wrapper
+
+
+P = ParamSpec("P")
+T = TypeVar("T")
+log = getLogger("backend_logger")
+
+
+class AppError(Exception):
+    def __init__(self, err: Exception | None) -> None:
+        self.original_error = err
+
+
+class InfraError(AppError):
+    def __init__(self, err: Exception) -> None:
+        super().__init__(err)
+        self.msg = "An error occurred in the infrastructure layer"
 
     def __str__(self) -> str:
-        return self.msg or "An error occurred in the infrastructure layer"
+        return self.msg
 
 
-class MaximumSpendError(Exception):
-    def __init__(self, msg: str | None = None) -> None:
-        self.msg = msg
-
-    def __str__(self) -> str:
-        return self.msg or "The maximum spend for the day has been reached"
-
-
-class InputError(Exception):
-    def __init__(self, msg: str | None = None) -> None:
-        self.msg = msg
+class MaximumSpendError(AppError):
+    def __init__(self, err: Exception) -> None:
+        super().__init__(err)
+        self.msg = "The maximum spend for the day has been reached"
 
     def __str__(self) -> str:
-        return self.msg or "Your input is invalid"
+        return self.msg
+
+
+class InputError(AppError):
+    def __init__(
+        self, message: str | None = None, err: Exception | None = None
+    ) -> None:
+        if message:
+            self.msg = message
+        else:
+            self.msg = "The input you provided is invalid"
+        super().__init__(err)
+
+    def __str__(self) -> str:
+        return self.msg
+
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+class map_errors(Generic[P, T]):
+    def __init__(self, func: Callable[P, T], map_to: Type[AppError] = InfraError):
+        self._func = func
+        self._map_to = map_to
+        update_wrapper(self, func)
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T:
+        try:
+            return self._func(*args, **kwargs)
+        except Exception as e:
+            log.error(f"Error in {self._func.__qualname__}: {e}", exc_info=True)
+            raise self._map_to(err=e) from e
