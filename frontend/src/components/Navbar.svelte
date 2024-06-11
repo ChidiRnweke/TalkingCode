8ec6a24a706e259a0057ce2c8a625e142803723f
ChidiRNweke <chidi125@gmail.com>
2024-06-11 09:10:18 +0200

    Add dark mode toggle to Navbar component

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -6,22 +6,28 @@
 	import NavLi from 'flowbite-svelte/NavLi.svelte';
 	import ArrowLeft from 'flowbite-svelte-icons/ArrowLeftToBracketOutline.svelte';
 	import Avatar from 'flowbite-svelte/Avatar.svelte';
+	import DarkMode from 'flowbite-svelte/DarkMode.svelte';
 </script>
 
 <Navbar>
 	<NavBrand href="/">
 		<Avatar src="/portrait.jpg" size="md" rounded border alt="Flowbite Logo" />
 		<span class="ml-2 self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-			>Chidi Nweke</span
+			>ChatGITpt</span
 		>
 	</NavBrand>
-	<NavHamburger />
-	<NavUl>
-		<NavLi href="/">Home</NavLi>
-		<NavLi href="/about">About</NavLi>
-		<NavLi href="/host-self">Host it yourself</NavLi>
-		<NavLi href="https://github.com/ChidiRnweke" class="flex flex-row place-items-center gap-x-1">
-			Source code <ArrowLeft size="md" />
-		</NavLi>
-	</NavUl>
+	<div class="flex flex-row">
+		<NavUl>
+			<NavLi href="/">Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/host-self">Host it yourself</NavLi>
+			<NavLi
+				href="https://github.com/ChidiRnweke/chatGITpt"
+				class="flex flex-row place-items-center gap-x-1"
+			>
+				Source code <ArrowLeft size="md" />
+			</NavLi>
+		</NavUl>
+		<DarkMode />
+	</div>
 </Navbar>
