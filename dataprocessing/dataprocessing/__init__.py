a969639adc44e9498e9ee7c89f1925aa3d2be158
ChidiRNweke <chidi125@gmail.com>
2024-06-15 16:40:24 +0200

    chore: Add migration script for running database migrations

--- dataprocessing/dataprocessing/__init__.py
+++ dataprocessing/dataprocessing/__init__.py
@@ -9,12 +9,23 @@ from dotenv import load_dotenv
 from dataprocessing.orchestration.resources import AppConfigResource
 from .orchestration import assets
 from shared.log import setup_custom_logger
-
-if not os.getenv("PRODUCTION"):
-    load_dotenv("../config/.env.secret.dev")
+from alembic.config import Config
+from alembic import command
 
 logger = setup_custom_logger("app_logger")
+if not os.getenv("PRODUCTION"):
+    logger.warning("Running in development mode")
+    load_dotenv("../config/.env.secret.dev")
 
+
+def run_migrations() -> None:
+    alembic_cfg = Config()
+    alembic_cfg.set_main_option("script_location", "../shared/shared/migrations")
+    command.upgrade(alembic_cfg, "head")
+    logger.info("Migrations run successfully")
+
+
+run_migrations()
 
 all_assets = load_assets_from_modules([assets])
 all_assets_job = define_asset_job(name="all_assets_job")
