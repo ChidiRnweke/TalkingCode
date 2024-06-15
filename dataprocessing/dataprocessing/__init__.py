3eea14c4e46870e8c786df3a2bfda2c8c8998a7f
ChidiRNweke <chidi125@gmail.com>
2024-06-15 13:55:11 +0200

    Add ingestion schedule for all assets job

--- dataprocessing/dataprocessing/__init__.py
+++ dataprocessing/dataprocessing/__init__.py
@@ -1,18 +1,31 @@
-from dagster import Definitions, load_assets_from_modules
+import os
+from dagster import (
+    Definitions,
+    ScheduleDefinition,
+    define_asset_job,
+    load_assets_from_modules,
+)
 from dotenv import load_dotenv
 from dataprocessing.orchestration.resources import AppConfigResource
 from .orchestration import assets
 from shared.log import setup_custom_logger
 
-load_dotenv("../config/.env.secret")
+if not os.getenv("PRODUCTION"):
+    load_dotenv("../config/.env.secret.dev")
 
 logger = setup_custom_logger("app_logger")
 
 
 all_assets = load_assets_from_modules([assets])
+all_assets_job = define_asset_job(name="all_assets_job")
 
+ingestion_schedule = ScheduleDefinition(
+    job=all_assets_job,
+    cron_schedule="0 0 * * *",
+)
 
 defs = Definitions(
     assets=all_assets,
     resources={"app_config_resource": AppConfigResource.from_env()},
+    schedules=[ingestion_schedule],
 )
