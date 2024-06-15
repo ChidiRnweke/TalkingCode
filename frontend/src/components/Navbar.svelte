aec964c8343528850612bc0133f84075f5ff5c72
ChidiRNweke <chidi125@gmail.com>
2024-06-16 01:45:20 +0200

    Let title specify that the project is in alpha

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import Navbar from 'flowbite-svelte/Navbar.svelte';
 	import NavBrand from 'flowbite-svelte/NavBrand.svelte';
-	import NavHamburger from 'flowbite-svelte/NavHamburger.svelte';
 	import NavUl from 'flowbite-svelte/NavUl.svelte';
 	import NavLi from 'flowbite-svelte/NavLi.svelte';
 	import ArrowLeft from 'flowbite-svelte-icons/ArrowLeftToBracketOutline.svelte';
@@ -13,14 +12,14 @@
 	<NavBrand href="/">
 		<Avatar src="/portrait.jpg" size="md" rounded border alt="Flowbite Logo" />
 		<span class="ml-2 self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-			>ChatGITpt</span
+			>ChatGITpt (Alpha)</span
 		>
 	</NavBrand>
 	<div class="flex flex-row">
 		<NavUl>
 			<NavLi href="/">Home</NavLi>
 			<NavLi href="/about">About</NavLi>
-			<NavLi href="/host-self">Host it yourself</NavLi>
+			<NavLi href="/host-it-yourself">Host it yourself</NavLi>
 			<NavLi
 				href="https://github.com/ChidiRnweke/chatGITpt"
 				class="flex flex-row place-items-center gap-x-1"
