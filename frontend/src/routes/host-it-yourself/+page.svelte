79e26d53bd409d0b1e26014fefbacd099476f977
ChidiRNweke <chidi125@gmail.com>
2024-06-16 01:43:50 +0200

    feat: Add "Host It Yourself" page template

---
+++ frontend/src/routes/host-it-yourself/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Heading from 'flowbite-svelte/Heading.svelte';
+	import Paragraph from '../../components/typography/Paragraph.svelte';
+</script>
+
+<article class="lg:mx-52 grid grid-cols-1 gap-y-10 mb-10">
+	<header>
+		<Heading>How to host it yourself.</Heading>
+	</header>
+
+	<section>
+		<Heading tag="h2" class="text-primary-700 text-xl mb-8">Under construction.</Heading>
+		<Paragraph>
+			Sorry, this page is still under construction. Please check back later. Feel free to reach out
+			to me on LinkedIn, GitHub, or even Instagram if you want to host this project yourself and I
+			haven't updated this page yet.
+		</Paragraph>
+	</section>
+</article>
