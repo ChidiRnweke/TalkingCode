d93d461a42dc25cef7815a8dc79bffa0a6fbdf10
ChidiRNweke <chidi125@gmail.com>
2024-06-16 11:39:22 +0200

    feat: Add 404 page template

---
+++ frontend/src/routes/404/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Heading from 'flowbite-svelte/Heading.svelte';
+	import Paragraph from '../../components/typography/Paragraph.svelte';
+</script>
+
+<article class="lg:mx-52 grid grid-cols-1 gap-y-10 mb-10">
+	<header>
+		<Heading>Page not found.</Heading>
+	</header>
+
+	<section>
+		<Heading tag="h2" class="text-primary-700 text-xl mb-8">404: Page not found.</Heading>
+		<Paragraph>
+			Sorry, this page does not exist. Please check the URL and try again. If you think this is a
+			mistake, please reach out to me on LinkedIn, GitHub, or even Instagram.
+		</Paragraph>
+	</section>
+</article>
