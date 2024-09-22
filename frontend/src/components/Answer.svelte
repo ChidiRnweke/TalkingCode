e6484f74498fe19f2b601a4f8eaabb0ab8e52acf
ChidiRNweke <chidi125@gmail.com>
2024-09-22 11:58:39 +0200

    Refactor Answer component styles for better text wrapping and overflow handling

--- frontend/src/components/Answer.svelte
+++ frontend/src/components/Answer.svelte
@@ -3,7 +3,7 @@
 	import Avatar from 'flowbite-svelte/Avatar.svelte';
 </script>
 
-<section class="flex flex-row mr-auto ml-0 lg:gap-x-10 gap-x-4 max-w-fit">
+<section class="flex flex-row mr-auto ml-0 lg:gap-x-10 gap-x-4 w-full">
 	<div>
 		<Avatar
 			src="/portrait.jpg"
@@ -13,7 +13,7 @@
 		/>
 		<P class="mt-2 text-center font-semibold text-primary-900">Chidi</P>
 	</div>
-	<P class="text-lg leading-10 text-wrap">
+	<P class="text-lg leading-10 break-words flex-grow">
 		<div class="generated">
 			<slot />
 		</div>
@@ -35,6 +35,7 @@
 		margin-left: 1rem;
 		margin-right: 1rem;
 		color: orangered;
+		white-space: pre-wrap;
 	}
 
 	:global(.dark) .generated :global(pre) {
@@ -70,5 +71,7 @@
 
 	.generated :global(p) {
 		margin-top: 1rem;
+		overflow-wrap: break-word;
+		word-break: break-all;
 	}
 </style>
