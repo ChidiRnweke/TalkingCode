d949844ccad41a2366577850d3e96413b95c6d4a
ChidiRNweke <chidi125@gmail.com>
2024-12-22 11:00:51 +0100

    Add SecretsReader class for improved secret management

--- shared/shared/env.py
+++ shared/shared/env.py
@@ -1,8 +1,54 @@
-import os
-from logging import Logger
-from .log import setup_custom_logger
-
+from logging import getLogger
 from dotenv import load_dotenv
+import os
+from dataclasses import dataclass
+from infisical_client import (
+    ClientSettings,
+    InfisicalClient,
+    GetSecretOptions,
+    AuthenticationOptions,
+    UniversalAuthMethod,
+)
+from typing import Self
+
+from shared.log import setup_custom_logger
+
+logger = getLogger("app_logger")
+
+
+@dataclass
+class SecretsReader:
+    client: InfisicalClient
+    project_id: str
+    environment: str
+
+    def read_secret(self, secret_name: str) -> str:
+        secret = self.client.getSecret(
+            options=GetSecretOptions(
+                environment=self.environment,
+                project_id=self.project_id,
+                secret_name=secret_name,
+            )
+        )
+        return secret.secret_value
+
+    @classmethod
+    def from_env(cls) -> Self:
+        client_id = get_env_or_raise("INFISICAL_CLIENT_ID")
+        client_secret = get_env_or_raise("INFISICAL_CLIENT_SECRET")
+        project_id = get_env_or_raise("INFISICAL_PROJECT_ID")
+        environment = get_env_or_raise("INFISICAL_ENVIRONMENT")
+        url = get_env_or_raise("INFISICAL_URL")
+
+        auth = UniversalAuthMethod(client_id=client_id, client_secret=client_secret)
+        auth_options = AuthenticationOptions(universal_auth=auth)
+        client_settings = ClientSettings(auth=auth_options, site_url=url)
+
+        return cls(
+            client=InfisicalClient(client_settings),
+            project_id=project_id,
+            environment=environment,
+        )
 
 
 def setup_env(logger_name: str) -> None:
@@ -14,22 +60,20 @@ def setup_env(logger_name: str) -> None:
             logger.warning("No .env file found")
 
 
-def env_var_or_default(var_name: str, default: str, log: Logger | None = None) -> str:
+def env_var_or_default(var_name: str, default: str) -> str:
     value = os.getenv(var_name)
     if value is None:
-        if log is not None:
-            log.warning(
-                f"{var_name} environment variable is not set. Fallback to default."
-            )
+        logger.warning(
+            f"{var_name} environment variable is not set. Fallback to default."
+        )
         value = default
     return value
 
 
-def env_var_or_throw(var_name: str, log: Logger | None = None) -> str:
+def get_env_or_raise(var_name: str) -> str:
     value = os.getenv(var_name)
     if value is None:
-        if log is not None:
-            log.error(f"{var_name} environment variable is not set. Cannot start app.")
+        logger.error(f"{var_name} environment variable is not set. Cannot start app.")
         raise ValueError(
             f"{var_name} environment variable is not set. Cannot start app."
         )
