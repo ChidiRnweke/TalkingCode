3005e43003d2a83e30cead58285e0d8855e07df3
ChidiRNweke <chidi125@gmail.com>
2024-12-24 12:02:47 +0100

    Add comments to make_models.bash for clarity on generated Pydantic models

--- pipelines/make_models.bash
+++ pipelines/make_models.bash
@@ -1,3 +1,15 @@
+#! /bin/bash
+
+# This script generates pydantic models for the github data from the json schemas
+
+
+# NOTE: The generated models do need some editing to make them work as the schemas are not perfect
+# and codegen seems to use an older version of pydantic. The AnyUrl type in particular is changed to
+# a string type in the generated models. This is not a problem as the type doesn't do much in the
+# generated models. On top of that, the `__root__` field is changed to the `root` field and `RootModel`
+# is changed to `BaseModel` in the generated models for those that have it. This is because the `__root__`
+# field is deprecated in the newer versions of pydantic.
+
 source .venv/bin/activate
 set -euxo pipefail
 input_folder="github_input_schemas"
