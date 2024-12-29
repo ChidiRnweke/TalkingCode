c44aeb3bc24628a4cbb455cadede4e43237946de
ChidiRNweke <chidi125@gmail.com>
2024-12-29 19:24:38 +0100

    Refactor MetadataIngestionService to use a single GitHub client instance for repository processing

--- pipelines/src/talkingcode/pipelines/processing/metadata_ingestion/ingestion.py
+++ pipelines/src/talkingcode/pipelines/processing/metadata_ingestion/ingestion.py
@@ -36,19 +36,16 @@ class MetadataIngestionService:
         await self._process_repositories()
 
     async def _process_repositories(self) -> None:
-        async with self.client as client:
-            user = client.get_user()
-            _repos = client.get_all_repositories()
-            (user, _repos) = await asyncio.gather(*[user, _repos])
-            _repos = [self._process_repository(user, repo, client) for repo in _repos]
-            await asyncio.gather(*_repos)
+        user = self.client.get_user()
+        _repos = self.client.get_all_repositories()
+        (user, _repos) = await asyncio.gather(*[user, _repos])
+        _repos = [self._process_repository(user, repo) for repo in _repos]
+        await asyncio.gather(*_repos)
 
-    async def _process_repository(
-        self, user: str, repo: "GitHubRepository", client: GitHubClient
-    ) -> None:
+    async def _process_repository(self, user: str, repo: "GitHubRepository") -> None:
         if repo.fork or repo.owner != user:
             return None
         logger.info(f"Processing repository {repo.name}")
-        files = await client.get_all_files(repo)
+        files = await self.client.get_all_files(repo)
         logger.info(f"Found {len(files)} files in {repo.name}")
         await self.db.write_to_database(repo, files)
