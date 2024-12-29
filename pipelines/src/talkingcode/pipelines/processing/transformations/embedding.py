b58ee98ef7eb73902b4cd0934a4159997dc666fb
ChidiRNweke <chidi125@gmail.com>
2024-12-29 19:24:53 +0100

    Handle empty file content in OpenAIEmbedder by returning a default message

--- pipelines/src/talkingcode/pipelines/processing/transformations/embedding.py
+++ pipelines/src/talkingcode/pipelines/processing/transformations/embedding.py
@@ -49,8 +49,8 @@ class OpenAIEmbedder(Embedder):
     embedding_model: str
 
     async def embed(self, file: FileMetadata) -> list[EmbeddedChunk]:
-        async with self.github as client:
-            file_content = await client.get_file_content(file.file)
+        file_content = await self.github.get_file_content(file.file)
+        file_content = "Empty file" if len(file_content) == 0 else file_content
         split_file_content = self.splitter.split_text_to_chunks(file_content)
         enriched_content = [
             self._enrich_file_content(chunk, file.file) for chunk in split_file_content
