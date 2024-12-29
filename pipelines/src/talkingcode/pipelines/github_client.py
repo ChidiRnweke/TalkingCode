9847b3a27b268da8ae05619f720fc96c05c909d5
ChidiRNweke <chidi125@gmail.com>
2024-12-29 19:24:12 +0100

    Spawn a HTTP client per action in github client
    Otherwise we run into trouble with resource management

--- pipelines/src/talkingcode/pipelines/github_client.py
+++ pipelines/src/talkingcode/pipelines/github_client.py
@@ -36,15 +36,12 @@ class GitHubClient(Protocol):
 
     async def get_user(self) -> str: ...
 
-    async def __aenter__(self) -> "GitHubClient": ...
-    async def __aexit__(self, exc_type, exc_val, exc_tb): ...
-
 
 @instrument_all_async(log_async_execution_time)
 @dataclass(frozen=True, slots=True)
 class GithubHTTPClient(GitHubClient):
     """
-    The `GithubHTTPClient` class is responsible for fetching data from the GitHub API.
+    The GithubHTTPClient class is responsible for fetching data from the GitHub API.
 
     Args:
         Args:
@@ -52,26 +49,20 @@ class GithubHTTPClient(GitHubClient):
     """
 
     auth_header: AuthHeader
-    client: AsyncClient = AsyncClient(timeout=500)
-
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.client.aclose()
 
     async def get_all_repositories(self) -> list[GitHubRepository]:
         """
         Fetches all repositories for the authenticated user.
 
         Returns:
-            list[GitHubRepository]: A list of `GitHubRepository` objects representing the repositories.
+            list[GitHubRepository]: A list of GitHubRepository objects representing the repositories.
         """
         header = self.auth_header.to_dict()
         path = "https://api.github.com/user/repos"
         language_result = []
         async with _github_semaphore:
-            response = await self.client.get(path, headers=header)
+            async with AsyncClient(timeout=500) as client:
+                response = await client.get(path, headers=header)
             _repos = response.json()
             repos = RepositoriesResponse(root=_repos)
         logger.info(
@@ -99,14 +90,16 @@ class GithubHTTPClient(GitHubClient):
         header = self.auth_header.to_dict()
         path = file.content_url
         async with _github_semaphore:
-            response = await self.client.get(path, headers=header)
+            async with AsyncClient(timeout=500) as client:
+                response = await client.get(path, headers=header)
             return response.text
 
     async def language_from_repo(self, repo: Repository) -> list[str]:
         header = self.auth_header.to_dict()
         path = repo.languages_url
         async with _github_semaphore:
-            response = await self.client.get(path, headers=header)
+            async with AsyncClient(timeout=500) as client:
+                response = await client.get(path, headers=header)
             langs_and_usage = Languages(root=response.json())
             if langs := langs_and_usage.root:
                 return list(langs.keys())
@@ -119,18 +112,19 @@ class GithubHTTPClient(GitHubClient):
         path = f"https://api.github.com/repos/{repo.user}/{repo.name}/git/trees/{repo.default_branch}?recursive=1"
 
         async with _github_semaphore:
-            response = await self.client.get(path, headers=header)
-            files = GitTree(**response.json())
-            file_paths = [file.path for file in files.tree if file.type == "blob"]
-            links = []
-            for file_path in file_paths:
-                content_path = f"https://api.github.com/repos/{repo.user}/{repo.name}/contents/{file_path}"
-                async with _github_semaphore:
-                    file = self.client.get(content_path, headers=header)
-                    links.append(file)
-            responses = await asyncio.gather(*links)
-            responses = [ContentTree(**response.json()) for response in responses]
-            logger.info(f"Found {len(responses)} files for {repo.name}")
+            async with AsyncClient(timeout=500) as client:
+                response = await client.get(path, headers=header)
+                files = GitTree(**response.json())
+                file_paths = [file.path for file in files.tree if file.type == "blob"]
+                links = []
+                for file_path in file_paths:
+                    content_path = f"https://api.github.com/repos/{repo.user}/{repo.name}/contents/{file_path}"
+                    async with _github_semaphore:
+                        file = client.get(content_path, headers=header)
+                        links.append(file)
+                responses = await asyncio.gather(*links)
+                responses = [ContentTree(**response.json()) for response in responses]
+                logger.info(f"Found {len(responses)} files for {repo.name}")
 
         return [
             GitHubFile(
@@ -147,20 +141,21 @@ class GithubHTTPClient(GitHubClient):
         header = self.auth_header.to_dict()
         path = "https://api.github.com/user"
         async with _github_semaphore:
-            response = await self.client.get(path, headers=header)
+            async with AsyncClient(timeout=500) as client:
+                response = await client.get(path, headers=header)
             return User(**response.json()).root.login
 
     @classmethod
     def from_config(cls, config: IngestionConfig) -> "GithubHTTPClient":
         """
-        Factory method to create an instance of the `GithubHTTPClient` class from a configuration object.
+        Factory method to create an instance of the GithubHTTPClient class from a configuration object.
 
 
         Args:
             config (IngestionConfig): The configuration object to use for creating the client.
 
         Returns:
-            GithubHTTPClient: An instance of the `GithubHTTPClient` class.
+            GithubHTTPClient: An instance of the GithubHTTPClient class.
         """
         header = AuthHeader(Authorization="Authorization", token=config.github_token)
         return cls(header)
