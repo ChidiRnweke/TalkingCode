1e39949fc1bdef45727362cf402262640975ac3c
ChidiRNweke <chidi125@gmail.com>
2024-12-31 13:14:41 +0100

    Configure logger earlier

--- pipelines/src/talkingcode/pipelines/app.py
+++ pipelines/src/talkingcode/pipelines/app.py
@@ -12,15 +12,6 @@ async def main() -> None:
 
 if __name__ == "__main__":
     app_logger = logging.getLogger("app_logger")
-    telemetry_enabled = bool(os.getenv("TELEMETRY_ENABLED"))
-    if telemetry_enabled:
-        telemetry_endpoint = os.getenv("TELEMETRY_ENDPOINT")
-        if not telemetry_endpoint:
-            raise ValueError(
-                "Telemetry endpoint is required when telemetry is enabled."
-            )
-        configure_telemetry(telemetry_endpoint)
-
     app_logger.setLevel(logging.DEBUG)
     if not app_logger.handlers:
         console_handler = logging.StreamHandler()
@@ -30,5 +21,14 @@ if __name__ == "__main__":
         )
         console_handler.setFormatter(formatter)
         app_logger.addHandler(console_handler)
+
+    telemetry_enabled = bool(os.getenv("TELEMETRY_ENABLED"))
+    if telemetry_enabled:
+        telemetry_endpoint = os.getenv("TELEMETRY_ENDPOINT")
+        if not telemetry_endpoint:
+            raise ValueError(
+                "Telemetry endpoint is required when telemetry is enabled."
+            )
+        configure_telemetry(telemetry_endpoint)
 
     asyncio.run(main())
